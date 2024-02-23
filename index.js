8e0c3a586b8a1e120eaec16ea41c2bf5138a44ea
v39a <134019055+v39a@users.noreply.github.com>
2024-02-23 16:12:52 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('messageCreate', async (message) => {
 
     const timezones = [
         { key: 'EST', value: ['GMT-12', 'GMT-11', 'GMT-10', 'GMT-9', 'GMT-8', 'GMT-7', 'GMT-6', 'GMT-5', 'GMT-4'] },
-        { key: 'GMT', value: ['GMT-3', 'GMT-2', 'GMT-1', 'GMT-0', 'GMT+1', 'GMT+2', 'GMT+3', 'GMT+4'] },
+        { key: 'GMT', value: ['GMT-3', 'GMT-2', 'GMT-1', 'GMT+0', 'GMT+1', 'GMT+2', 'GMT+3', 'GMT+4'] },
         { key: 'AEST', value: ['GMT+5', 'GMT+6', 'GMT+7', 'GMT+8', 'GMT+9', 'GMT+10', 'GMT+11', 'GMT+12', 'GMT+13'] },
     ];
 
